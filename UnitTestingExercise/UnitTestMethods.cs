c291468bdbde01c4641885bd245adaf9d028f27b
jcrowe91 <jcrowe9119@gmail.com>
2022-01-27 12:22:03 -0600

    Built methods

--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -4,23 +4,48 @@ namespace UnitTestingExercise
     public class UnitTestMethods
     {
         // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // Righ-click on Dependencies in your .Tests project and set your reference
 
 
 
         // Create an Add method that passes 3 integers
+        public void Add(int first, int second, int third)
+        {
+
+        }
 
         // Create a Subtract method that passes 2 integers
             // Keep track of which number is getting passed as minuend and subtrahend
+            public void Subtract(int first, int second)
+        {
+            int minuiend = first;
+            int subtrahend = second;
+        }
 
         // Create a Multiply method that passes 2 integers
+        public void Multiply(int first, int second)
+        {
+
+        }
 
         // Create a Divide method that passes 2 integers
+        public void Divide(int first, int second)
+        {
+
+        }
 
 
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
+        public void IsEven(int number)
+        {
 
+        }
+
+        public void IsOdd(int number)
+        {
+
+        }
         
     }
 }
